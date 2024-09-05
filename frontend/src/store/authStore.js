c2e5bfa650fec9ec11110dfdc0f0c6d492fc243c
Sayan Biswas <sayanbiswas2468@gmail.com>
2024-09-05 23:35:16 +0530

    deployment

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -18,7 +18,7 @@ export const useAuthStore = create((set) => ({
         try {
             const response = await axios.post(`${API_URL}/signup`, { email, password, name });
             set({ user: response.data.user, isAuthenticated: true, isLoading: false });
-        } catch (error) {
+        } catch (error) { 
             set({ error: error.response.data.message || "Error signing up", isLoading: false });
             throw error;
         }
