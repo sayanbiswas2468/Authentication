45c81eee521ee0799bc87a2a4f9d2ffc46ff21f4
Sayan Biswas <sayanbiswas2468@gmail.com>
2024-09-05 21:42:44 +0530

    complete frontend

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,35 @@
-import express from 'express'
-import { connectDB } from './db/connectDB.js';
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser';
-import authRoutes from './routes/auth.route.js'
-dotenv.config();
-const app = express();
-const PORT = process.env.PORT || 5000
-app.use(express.json())
-app.use(cookieParser())
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
 
-app.use("/api/auth", authRoutes)
+import { connectDB } from "./db/connectDB.js";
+
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+app.use(express.json()); 
+app.use(cookieParser()); 
+
+app.use("/api/auth", authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
+
 app.listen(PORT, () => {
     connectDB();
-    console.log(`Server is running on ${PORT}`);
-})
+    console.log("Server is running on port: ", PORT);
+});
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,12 +1,19 @@
-import { motion } from 'framer-motion'
-import Input from '../components/Input'
-import { Lock, Mail, User } from 'lucide-react'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { motion } from "framer-motion";
+import Input from "../components/Input";
+import { Loader, Lock, Mail, User } from "lucide-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import PasswordStrengthMeter from "../components/PasswordStrengthMeter";
+import { useAuthStore } from "../store/authStore";
+
 const SignUpPage = () => {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const { signup, error, isLoading } = useAuthStore();
+
     const handleSignUp = async (e) => {
         e.preventDefault();
 
@@ -22,34 +29,38 @@ const SignUpPage = () => {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
-            className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden'
+            className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl 
+			overflow-hidden'
         >
             <div className='p-8'>
                 <h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text'>
                     Create Account
                 </h2>
+
                 <form onSubmit={handleSignUp}>
                     <Input
                         icon={User}
                         type='text'
-                        placeholder="Full Name"
+                        placeholder='Full Name'
                         value={name}
                         onChange={(e) => setName(e.target.value)}
                     />
                     <Input
                         icon={Mail}
                         type='email'
-                        placeholder="Email Address"
+                        placeholder='Email Address'
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                     />
                     <Input
                         icon={Lock}
                         type='password'
-                        placeholder="Password"
+                        placeholder='Password'
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
+                    {error && <p className='text-red-500 font-semibold mt-2'>{error}</p>}
+                    <PasswordStrengthMeter password={password} />
 
                     <motion.button
                         className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
@@ -59,22 +70,21 @@ const SignUpPage = () => {
                         whileHover={{ scale: 1.02 }}
                         whileTap={{ scale: 0.98 }}
                         type='submit'
-                    // disabled={isLoading}
+                        disabled={isLoading}
                     >
-                        Sign Up
+                        {isLoading ? <Loader className=' animate-spin mx-auto' size={24} /> : "Sign Up"}
                     </motion.button>
                 </form>
             </div>
-            <div className='px-8 py-4 bg-gray-900 bg-opacity-50 justify-center'>
-                <p>
+            <div className='px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center'>
+                <p className='text-sm text-gray-400'>
                     Already have an account?{" "}
-                    <Link to={'/login'} className='text-green-400 hover:underline'>
+                    <Link to={"/login"} className='text-green-400 hover:underline'>
                         Login
                     </Link>
                 </p>
             </div>
-
         </motion.div>
-    )
-}
-export default SignUpPage
+    );
+};
+export default SignUpPage;
